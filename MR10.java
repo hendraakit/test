3652cf45f0256d305e90d3be971f1a09b4af5ca3
hendraakit <akit4277@gmail.com>
2016-06-17 22:48:41 +0700

    Add files via upload
    Menu Testcases

---
+++ MR10.java
@@ -0,0 +1,28 @@
+package MR10;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class MR10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.practiceselenium.com/menu.html");
+		WebElement element = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914882"));
+		element.click();
+		WebElement title = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000453230000"));
+		String x = "title";
+		 if(x == "title")
+		 {System.out.println("Pass");}
+		 else{System.out.println("failed");}
+		 
+
+
+	}
+
+}
