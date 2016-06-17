5e6762fdf20eeb97d6b006996dac0e003648e969
hendraakit <akit4277@gmail.com>
2016-06-17 22:47:04 +0700

    Add files via upload
    Check out test cases

---
+++ CR1.java
@@ -0,0 +1,28 @@
+package CR1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class CR1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.practiceselenium.com/check-out.html");
+		WebElement element = driver.findElement(By.linkText("Our Passion"));
+		element.click();
+		WebElement title = driver.findElement(By.className("editor_h1"));
+		String x = "title";
+		 if(x == "title")
+		 {System.out.println("Pass");}
+		 else{System.out.println("failed");}
+		 
+
+
+	}
+
+}
---
+++ CR3.java
@@ -0,0 +1,28 @@
+package CR3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class CR3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.practiceselenium.com/check-out.html");
+		WebElement element = driver.findElement(By.linkText("Menu"));
+		element.click();
+		WebElement title = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000451934628"));
+		String x = "title";
+		 if(x == "title")
+		 {System.out.println("Pass");}
+		 else{System.out.println("failed");}
+		 
+
+
+	}
+
+}
