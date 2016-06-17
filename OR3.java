2f0e0d8ae14a4cd12ba0780a6abb781ba8e10be6
hendraakit <akit4277@gmail.com>
2016-06-17 22:49:43 +0700

    Add files via upload
    WR9

---
+++ OR3.java
@@ -0,0 +1,28 @@
+package OR3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class OR3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.practiceselenium.com/our-passion.html");
+		WebElement element = driver.findElement(By.linkText("Let's Talk Tea"));
+		element.click();
+		WebElement title = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914916"));
+		String x = "title";
+		 if(x == "title")
+		 {System.out.println("Pass");}
+		 else{System.out.println("failed");}
+		 
+
+
+	}
+
+}
